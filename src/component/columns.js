dc72354befca55817dc040b49b5a87a89beb4b57
OshigeAkito <88414912+OshigeAkito@users.noreply.github.com>
2022-07-02 09:15:02 +0700

    firstpush

---
+++ src/component/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS= [
+    {
+        Header: 'Id',  /*header of table app*/
+        accessor: 'id'  /*source column name*/
+    },
+    {
+        Header: 'First Name', 
+        accessor: 'first_name'    
+    },
+    {
+        Header: 'Last Namee', 
+        accessor: 'last_name'   
+    },
+    {
+        Header: 'Date of Birth',
+        accessor: 'date_of_birth'   
+    },
+    {
+        Header: 'Country', 
+        accessor: 'country'    
+    },
+    {
+        Header: 'Phone',   
+        accessor: 'phone'   
+    },
+]
